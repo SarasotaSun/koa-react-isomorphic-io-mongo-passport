055c208c11807b40f255b440324021037cd7d234
SarasotaSun <srqseo2@yahoo.com>
2015-10-04 08:51:23 -0400

    intial

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class NotFound extends Component {
   render() {
-    return (<h1>Error: Page Not Found ...</h1>);
+    return (<h1>Error: Page Not Found ....</h1>);
   }
 }
