f4e093b051c6edbc9a4334afbb119bbf847bf2bd
SarasotaSun <srqseo2@yahoo.com>
2015-10-04 20:21:47 -0400

    intial

---
+++ src/components/parts/new-Questions.js
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Questions extends Component {
+
+  static propTypes = {
+    emit: PropTypes.func,
+    questions: PropTypes.array
+  };
+
+constructor() {
+  super();
+}
+
+ ask(question) {
+   this.props.emit('ask', { question: question });
+ }
+
+  addQuestion(question, index) {
+    return (
+      <div key={ index } className="col-xs-12 col-sm-6 col-md-3">
+        <span onClick={ () => this.ask(question) }>{ question.q }</span>
+      </div>
+    );
+  }
+
+  render() {
+    const questions = this.props.questions;
+    return (
+      <div id="questions" className="row">
+        <h2>Questions</h2>
+        { questions.map(this.addQuestion.bind(this)) }
+      </div>
+    );
+  }
+}
